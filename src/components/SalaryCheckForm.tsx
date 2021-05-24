78435f76e995ff6a71d8595c08eb94162e84c9fd
Carl Upton <carlupton15@gmail.com>
2021-05-24 16:22:15 +0100

    got a very simple form working

--- src/components/SalaryCheckForm.tsx
+++ src/components/SalaryCheckForm.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  salary: Number;
+};
 
 export default function SalaryCheckForm() {
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
-    <span>hello</span>
+    <form onSubmit={onSubmit}>
+      <label htmlFor="salary-input">
+        Salary
+        <input id="salary-input" type="number" {...register('salary')} />
+      </label>
+      <input type="submit" />
+    </form>
   );
 }
